6b04546449d90008eea347b6c8f876384c0f918b
s2ddv <1speedftn@gmail.com>
2025-09-04 15:04:02 -0300

    chore: finish setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => { 
+    return <div className="min-h-screen">
+        
+        {/* Theme Toggle */}
+
+        {/* Background Effects */}
+
+        {/* Nav Bar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+    </div>;
+}
